ea92871be7063838ddeb6529d7f0fdae2bbd4063
Wolfram Schneider <wosch@indexdata.dk>
2013-08-21 18:04:21 +0200

    refactor to use local vars

--- test/spec/jsdom.spec.js
+++ test/spec/jsdom.spec.js
@@ -4,13 +4,14 @@
  *
  */
 
-var jsdom = require('jsdom').jsdom;
-var myWindow = jsdom().createWindow();
-var $ = require('jquery');
-var jq = require('jquery').create();
-var jQuery = require('jquery').create(myWindow);
-
 describe("jQuery suite", function() {
+  var jsdom = require('jsdom').jsdom;
+  var myWindow = jsdom().createWindow();
+
+  var $ = require('jquery');
+  var jq = require('jquery').create();
+  var jQuery = require('jquery').create(myWindow);
+
   it("jQuery append test", function() {
     jQuery("<h1>test passes h1</h1>").appendTo("body");
     expect( jQuery("body").html() ).toMatch(/<h1>/);
@@ -26,9 +27,6 @@ describe("jQuery suite", function() {
     expect( jq("body").html() ).toMatch(/<h2>/);
   });
 
-});
-
-describe("jsdom suite", function() {
   it("window test", function() {
     expect(myWindow).toBeDefined();
   });
