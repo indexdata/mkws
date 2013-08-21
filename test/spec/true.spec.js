2394e6527edfe222a73fa046c57c4ccd34350ba9
Wolfram Schneider <wosch@indexdata.dk>
2013-08-21 18:00:12 +0200

    documentation

--- test/spec/true.spec.js
+++ test/spec/true.spec.js
@@ -1,4 +1,8 @@
-
+/* Copyright (c) 2013 IndexData ApS. http://indexdata.com
+ *
+ * dummy test
+ *
+ */
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
