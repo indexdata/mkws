a42495ef19e9aa2591a8fe5d05f1fb23cc27cf86
Wolfram Schneider <wosch@indexdata.dk>
2013-08-22 11:28:55 +0000

    jquery.pazpar2() will handle the option id_popup

--- tools/htdocs/mkws.js
+++ tools/htdocs/mkws.js
@@ -842,6 +842,9 @@ $.extend({
 
     // service-proxy or pazpar2
     pazpar2: function(config) {
+	var id_popup = config.id_popup || "#mkwsPopup";
+	id_popup = id_popup.replace(/^#/, "");
+
 	// simple layout
 	var div = '<div id="mkwsSwitch"></div>\
 	<div id="mkwsLang"></div>\
@@ -898,7 +901,7 @@ $.extend({
 
 	var popup = '\
 	  <div id="mkwsSearch"></div>\
-	  <div id="mkwsPopup">\
+	  <div id="' + id_popup + '">\
 	    <div id="mkwsSwitch"></div>\
 	    <div id="mkwsLang"></div>\
 	    <div id="mkwsResults"></div>\
@@ -910,7 +913,7 @@ $.extend({
 	    this.debug2("jquery plugin layout: div");
 	    document.write(div);
 	} else if (config && config.layout == 'popup') {
-	    this.debug2("jquery plugin layout: popup");
+	    this.debug2("jquery plugin layout: popup with id: " + id_popup);
 	    document.write(popup);
 	    $(document).ready( function() { init_popup(config); } );
 	} else {
