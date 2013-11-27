5e5012f066395e53b3c11ac41aa5e01549cbb3cc
Mike Taylor <mike@indexdata.com>
2013-11-27 16:20:37 +0000

    Add Handlebars helper for #link. Initial version straight from documentation at http://handlebarsjs.com/#block-expressions
    Doesn't work correctly with arrays. To be fixed in next delta.

--- tools/htdocs/mkws.js
+++ tools/htdocs/mkws.js
@@ -3,6 +3,15 @@
 
 "use strict"; // HTML5: disable for debug_level >= 2
 
+Handlebars.registerHelper('link', function(text, url) {
+  text = Handlebars.Utils.escapeExpression(text);
+  url  = Handlebars.Utils.escapeExpression(url);
+
+  var result = '<a href="' + url + '">' + text + '</a>';
+
+  return new Handlebars.SafeString(result);
+});
+
 // Set up namespace and some state.
 var mkws = {
     filters: [],
