bd512b55ee9b9bb1d19633d7242b9c36ec4c08e0
Mike Taylor <mike@indexdata.com>
2013-11-28 16:25:33 +0000

    Documentation for the #if-any helper.

--- tools/htdocs/mkws.js
+++ tools/htdocs/mkws.js
@@ -124,6 +124,12 @@ Handlebars.registerHelper('attr', function(attrName) {
 });
 
 
+/*
+ * Use as follows: {{#if-any NAME1 having="NAME2"}}
+ * Applicable when NAME1 is the name of an array
+ * The guarded code runs only if at least one element of the NAME1
+ * array has a subelement called NAME2.
+ */
 Handlebars.registerHelper('if-any', function(items, options) {
     var having = options.hash.having;
     for (var i in items) {
