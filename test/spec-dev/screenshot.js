31c55ebec20636cde992491aca72d83d237c4211
Wolfram Schneider <wosch@indexdata.dk>
2014-01-30 17:38:49 +0000

    indent

--- test/spec-dev/screenshot.js
+++ test/spec-dev/screenshot.js
@@ -1,12 +1,9 @@
 // run: phantomjs /path/to/this/file
-
 var url = 'http://www.indexdata.com/';
 var file_png = "indexdata.png";
 
 var page = require('webpage').create();
-
 page.open(url, function () {
     page.render(file_png);
     phantom.exit();
 });
-
