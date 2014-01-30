552dacefd4ad7dfffad2d2784aa340916e8d2587
Wolfram Schneider <wosch@indexdata.dk>
2014-01-30 18:31:59 +0000

    render indexdata homepage in more realistic 960x800 pixels

--- test/spec-dev/screenshot.js
+++ test/spec-dev/screenshot.js
@@ -3,7 +3,18 @@ var url = 'http://www.indexdata.com/';
 var file_png = "indexdata.png";
 
 var page = require('webpage').create();
+
+// page.paperSize = { format: 'A4', orientation: "landscape" };
+page.viewportSize = {
+    width: 960,
+    height: 800
+};
+page.zoomFactor = 1.0;
+
 page.open(url, function () {
-    page.render(file_png);
-    phantom.exit();
+    // small delay
+    setTimeout(function () {
+        page.render(file_png);
+        phantom.exit();
+    }, 500);
 });
