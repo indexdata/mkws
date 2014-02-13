2868bb6174a1c6d7209c621513ab15386428ce9a
Mike Taylor <mike@indexdata.com>
2014-02-13 10:56:06 +0000

    Add logging for popup.

--- tools/htdocs/mkws-jquery.js
+++ tools/htdocs/mkws-jquery.js
@@ -31,6 +31,7 @@ function _mkws_jquery_plugin ($) {
 	    return;
 	}
 
+	debug("Calling dialog()");
 	$(id_popup).dialog({
 	    closeOnEscape: true,
 	    autoOpen: false,
@@ -45,10 +46,12 @@ function _mkws_jquery_plugin ($) {
 	    },
 	    close: function() { }
 	});
+	debug("Called dialog()");
 
 	$(id_button)
 	    .button()
 	    .click(function() {
+		debug("Opening dialog()");
 		$(id_popup).dialog("open");
 	    });
     };
