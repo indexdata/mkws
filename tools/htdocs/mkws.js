ec95c20272197409599daa60edd48298a72312a4
Mike Taylor <mike@indexdata.com>
2014-02-20 16:38:54 -0500

    Comment on use of SP.

--- tools/htdocs/mkws.js
+++ tools/htdocs/mkws.js
@@ -503,6 +503,7 @@ function team($, teamName) {
 	debug("triggerSearch(" + m_query + "): filters = " + $.toJSON(m_filters) + ", pp2filter = " + pp2filter + ", params = " + $.toJSON(params));
 
 	// We can use: params.torusquery = "udb=NAME"
+	// Note: that won't work when running against raw pazpar2
 	m_paz.search(m_query, m_perpage, m_sort, pp2filter, undefined, params);
     }
 
