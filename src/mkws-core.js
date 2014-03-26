b9ec634af85ea55b6241d4ffe1c23189a2840f26
Mike Taylor <mike@indexdata.com>
2014-03-26 11:22:38 +0000

    Add comment

--- src/mkws-core.js
+++ src/mkws-core.js
@@ -161,6 +161,9 @@ if (mkws_config == null || typeof mkws_config != 'object') {
     };
 
 
+    // The following functions are dispatchers for team methods that
+    // are called from the UI using a te3am-name rather than implicit
+    // context.
     mkws.switchView = function(tname, view) {
 	mkws.teams[tname].switchView(view);
     }
