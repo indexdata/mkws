ace07b35d097150fbe6c3244bf4bfea4a0c293fa
Mike Taylor <mike@indexdata.com>
2014-03-31 17:19:46 +0100

    New file, mkws-widget-log.js

---
+++ src/mkws-widget-log.js
@@ -0,0 +1,7 @@
+mkws.registerWidgetType('Log', function() {
+    var that = this;
+
+    this.team.queue("log").subscribe(function(teamName, timestamp, message) {
+	$(that.node).append(teamName + ": " + timestamp + message + "<br/>");
+    });
+});
