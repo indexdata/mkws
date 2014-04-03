9ea3766d69749cbe9a6ac3302e57327ffe123c85
Mike Taylor <mike@indexdata.com>
2014-04-03 17:19:11 +0100

    Add "GoogleImage" subclass widget.

--- src/mkws-widget-image.js
+++ src/mkws-widget-image.js
@@ -2,3 +2,8 @@ mkws.registerWidgetType('Image', function() {
     mkws.promotionFunction('Record').call(this);
     if (!this.config.template) this.config.template = 'Image';
 });
+
+mkws.registerWidgetType('GoogleImage', function() {
+    mkws.promotionFunction('Image').call(this);
+    if (!this.config.target) this.config.target = 'Google_Images';
+});
