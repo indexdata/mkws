5c464ebaabf6169481b587ad1e3fc119dcafafa9
Mike Taylor <mike@indexdata.com>
2014-04-04 16:40:11 +0100

    Rename variable.

--- src/mkws-widget-termlists.js
+++ src/mkws-widget-termlists.js
@@ -52,26 +52,26 @@ mkws.registerWidgetType('Termlists', function() {
 	function makeSingleFacet(caption, data, max, pzIndex) {
 	    var teamName = that.team.name();
 
-	    var a2 = [];
-	    a2.push('<div class="termtitle">' + M(caption) + '</div>');
+	    var acc = [];
+	    acc.push('<div class="termtitle">' + M(caption) + '</div>');
 	    for (var i = 0; i < data.length && i < max; i++) {
-		a2.push('<div class="term">');
-		a2.push('<a href="#" ');
+		acc.push('<div class="term">');
+		acc.push('<a href="#" ');
 		var action = '';
 		if (!pzIndex) {
 		    // Special case: target selection
-		    a2.push('target_id='+data[i].id+' ');
+		    acc.push('target_id='+data[i].id+' ');
 		    if (!that.team.targetFiltered(data[i].id)) {
 			action = 'mkws.limitTarget(\'' + teamName + '\', this.getAttribute(\'target_id\'),this.firstChild.nodeValue)';
 		    }
 		} else {
 		    action = 'mkws.limitQuery(\'' + teamName + '\', \'' + pzIndex + '\', this.firstChild.nodeValue)';
 		}
-		a2.push('onclick="' + action + ';return false;">' + data[i].name + '</a>'
+		acc.push('onclick="' + action + ';return false;">' + data[i].name + '</a>'
 			 + ' <span>' + data[i].freq + '</span>');
-		a2.push('</div>');
+		acc.push('</div>');
 	    }
-	    return a2.join('');
+	    return acc.join('');
 	}
     });
 
