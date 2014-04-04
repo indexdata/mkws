7e9bf49237333c81ce6946ee735ba454dd0198b3
Mike Taylor <mike@indexdata.com>
2014-04-04 16:12:07 +0100

    We now use a silly half-and-half scheme where an accumulator fills up the scaffolding HTML while we also gather separately the HTML of each individual facet. Working towards facet separation.

--- src/mkws-widget-termlists.js
+++ src/mkws-widget-termlists.js
@@ -20,6 +20,7 @@ mkws.registerWidgetType('Termlists', function() {
 	// display if we first got results
 	$(that.node).show();
 
+	var output = {};
 	var acc = [];
 	acc.push('<div class="title">' + M('Termlists') + '</div>');
 
@@ -29,34 +30,46 @@ mkws.registerWidgetType('Termlists', function() {
 	    if (!ref) {
 		alert("bad facet configuration: '" + name + "'");
 	    } else {
-		addSingleFacet(acc, ref[0], data[name], ref[1], ref[2] ? name : null);
+		output[name] = addSingleFacet(acc, ref[0], data[name], ref[1], ref[2] ? name : null);
 	    }
 	}
 
 	$(that.node).html(acc.join(''));
 
+	for (var i = 0; i < facets.length; i++) {
+	    var name = facets[i]
+	    var ref = facetConfig[name];
+	    var caption = ref[0];
+
+	    that.log("setting HTML for facet '" + name + "'");
+	    that.team.findnode('.mkwsFacet' + caption).html(output[name]);
+	}
+
 	function addSingleFacet(acc, caption, data, max, pzIndex) {
 	    var teamName = that.team.name();
 	    acc.push('<div class="mkwsFacet mkwsFacet' + caption + ' mkwsTeam_' + teamName + '">');
-	    acc.push('<div class="termtitle">' + M(caption) + '</div>');
+
+	    var a2 = [];
+	    a2.push('<div class="termtitle">' + M(caption) + '</div>');
 	    for (var i = 0; i < data.length && i < max; i++) {
-		acc.push('<div class="term">');
-		acc.push('<a href="#" ');
+		a2.push('<div class="term">');
+		a2.push('<a href="#" ');
 		var action = '';
 		if (!pzIndex) {
 		    // Special case: target selection
-		    acc.push('target_id='+data[i].id+' ');
+		    a2.push('target_id='+data[i].id+' ');
 		    if (!that.team.targetFiltered(data[i].id)) {
 			action = 'mkws.limitTarget(\'' + teamName + '\', this.getAttribute(\'target_id\'),this.firstChild.nodeValue)';
 		    }
 		} else {
 		    action = 'mkws.limitQuery(\'' + teamName + '\', \'' + pzIndex + '\', this.firstChild.nodeValue)';
 		}
-		acc.push('onclick="' + action + ';return false;">' + data[i].name + '</a>'
+		a2.push('onclick="' + action + ';return false;">' + data[i].name + '</a>'
 			 + ' <span>' + data[i].freq + '</span>');
-		acc.push('</div>');
+		a2.push('</div>');
 	    }
 	    acc.push('</div>');
+	    return a2.join('');
 	}
     });
 
