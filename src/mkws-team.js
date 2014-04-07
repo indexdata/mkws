7baf039a695c86ec3a113accebf57ecb15f38ba2
Mike Taylor <mike@indexdata.com>
2014-04-07 16:26:07 +0100

    Add as-yet-unused data-mkws-facet attribute to mkwsFacet elements.

--- src/mkws-team.js
+++ src/mkws-team.js
@@ -442,7 +442,7 @@ function team($, teamName) {
 	    if (!ref) {
 		alert("bad facet configuration: '" + name + "'");
 	    } else {
-		acc.push('<div class="mkwsFacet' + ref[0] + ' mkwsFacet mkwsTeam_' + m_teamName + '">');
+		acc.push('<div class="mkwsFacet' + ref[0] + ' mkwsFacet mkwsTeam_' + m_teamName + '" data-mkws-facet="' + ref[0] + '">');
 		acc.push('</div>');
 	    }
 	}
