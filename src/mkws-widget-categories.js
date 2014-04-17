6d77d26b06a5a066405c63ca02369cd9f30ed46e
Mike Taylor <mike@indexdata.com>
2014-04-17 17:13:38 +0100

    Not quite a trivial Categories widget any more.

--- src/mkws-widget-categories.js
+++ src/mkws-widget-categories.js
@@ -1,1 +1,24 @@
-var req = new pzHttpRequest(auth.servicePath + "?command=categories");
+mkws.registerWidgetType('Categories', function() {
+    var that = this;
+
+    if (!this.config.use_service_proxy) {
+	alert("can't use categories widget without Service Proxy");
+	return;
+    }
+
+    this.team.queue("authenticated").subscribe(function(authName, realm) {
+	$(that.node).append("<p><b>Categories for " + realm + "</b></p>");	
+	var req = new pzHttpRequest(that.config.pazpar2_url + "?command=categories", function(err) {
+	    alert("HTTP call for categories failed: " + err)
+	});
+
+	req.get(null, function(data) {
+	    if (!$.isXMLDoc(data)) {
+		alert("categories response document is not XML");
+		return;
+	    }
+	    that.log("got categories: " + data);
+	    // Parse once we've figured out the format
+	});
+    });
+});
