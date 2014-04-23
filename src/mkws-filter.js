681a6de3a7f4d1c9493d66157b11c948c59434ae
Mike Taylor <mike@indexdata.com>
2014-04-23 14:43:34 +0100

    filter factory accepts name argument.

--- src/mkws-filter.js
+++ src/mkws-filter.js
@@ -1,5 +1,36 @@
+// Factory function for sets of filters.
+function filterSet() {
+    var that = {};
+    var m_list = [];
+
+    that.list = function() {
+	return m_list;
+    };
+
+    that.add = function(filter) {
+	m_list.push(filter);
+    };
+
+    that.removeMatching = function(matchFn) {
+	var newList = [];
+	for (var i in m_list) {
+	    var filter = m_list[i];
+	    if (matchFn(filter)) {
+		log("removeMatching() removing filter " + $.toJSON(filter));
+	    } else {
+		log("removeMatching() keeping filter " + $.toJSON(filter));
+		newList.push(filter);
+	    }
+	}
+	m_list = newList;
+    };
+
+    return that;
+}
+
+
 // Factory function for filters. These can be of several types.
-function filter(id, field, value) {
+function filter(id, name, field, value) {
     var res;
 
     if (id) {
