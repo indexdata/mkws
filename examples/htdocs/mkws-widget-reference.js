08c8459218566eb5c56ba9a098f3079ffc26e973
Mike Taylor <mike@indexdata.com>
2014-05-07 13:28:04 +0100

    Re-indent.

--- examples/htdocs/mkws-widget-reference.js
+++ examples/htdocs/mkws-widget-reference.js
@@ -1,9 +1,9 @@
 mkws.registerWidgetType('Reference', function() {
-    mkws.promotionFunction('Record').call(this);
-    if (!this.config.target) this.config.target = 'wikimedia_wikipedia_single_result';
-    if (!this.config.template) this.config.template = 'Reference';
+  mkws.promotionFunction('Record').call(this);
+  if (!this.config.target) this.config.target = 'wikimedia_wikipedia_single_result';
+  if (!this.config.template) this.config.template = 'Reference';
 
-    this.team.registerTemplate('Reference', '\
+  this.team.registerTemplate('Reference', '\
   <img src="{{md-thumburl}}" alt="{{md-title}}">\
   <h1><a href="{{md-electronic-url}}">{{md-title}}</a></h1>\
 {{#if md-title-remainder}}\
