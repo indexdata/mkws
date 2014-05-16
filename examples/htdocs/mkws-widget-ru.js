3aa819ed8eaa4b37333a105c8e266c1e8e1ff2bb
Jason Skomorowski <jason@indexdata.com>
2014-05-16 00:59:28 -0400

    Reference Universe widget MKWS-156

---
+++ examples/htdocs/mkws-widget-ru.js
@@ -0,0 +1,11 @@
+mkws.registerWidgetType('ReferenceUniverse', function() {
+    //this.team.config.service_proxy_auth = "http://mkws.indexdata.com/service-proxy/?command=auth&action=login&username=paratext&password=paratext_mkc";
+    // this.team.config.perpage_default = 5;
+    // this.team.config.sort_default = "position";
+    var teamClass = 'mkwsTeam_' + this.team.name();
+    var html = "<h2>Reference Universe results:</h2>\n";
+    html += '<div class="mkwsRecords ' + teamClass 
+            + '" autosearch="' + $(this.node).attr("autosearch")
+            + '" sort="position" perpage="5"></div>';
+    $(this.node).html(html);
+});
