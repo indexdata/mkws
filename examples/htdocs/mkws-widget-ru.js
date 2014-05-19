17ac8f1707ae8b1dd20c78c7b0935822e4fc60f1
Jason Skomorowski <jason@indexdata.com>
2014-05-19 17:27:34 -0400

    RU widget: use own template; be independent of records widget

--- examples/htdocs/mkws-widget-ru.js
+++ examples/htdocs/mkws-widget-ru.js
@@ -1,23 +1,33 @@
 var mkws_config = {service_proxy_auth: "http://mkws.indexdata.com/service-proxy/?command=auth&action=login&username=paratext&password=paratext_mkc"};
-console.log(JSON.stringify(mkws.config));
 
 mkws.registerWidgetType('ReferenceUniverse', function() {
   if (!this.config.perpage) this.config.perpage = 5;
   if (!this.config.sort) this.config.sort = "position";
-  this.config.template = 'ReferenceUniverse';
   this.team.registerTemplate('ReferenceUniverse', '\
-{{#mkws-first md-electronic-url}}\
-<a href="{{this}}">\
-{{/mkws-first}}\
-  {{md-title}}\
-</a>\
-{{#if md-title-remainder}}\
-  <span>{{md-title-remainder}}</span>\
-{{/if}}\
-{{#if md-title-responsibility}}\
-  <span><i>{{md-title-responsibility}}</i></span>\
-{{/if}}\
+<h2>Results from Reference Universe</h2>\
+<ul>\
+{{#each hits}}\
+  <li>\
+    {{#mkws-first md-electronic-url}}\
+    <a href="{{this}}">\
+    {{/mkws-first}}\
+      {{md-title}}\
+    </a>\
+  {{#if md-title-remainder}}\
+    <span>{{md-title-remainder}}</span>\
+  {{/if}}\
+  {{#if md-title-responsibility}}\
+    <span><i>{{md-title-responsibility}}</i></span>\
+  {{/if}}\
+  </li>\
+{{/each}}\
+</ul>\
 ');
-  this.node.html("<h2>Reference Universe results:</h2>\n" +
-                 this.subwidget('Records'));
+
+  var that = this;
+  var template = that.team.loadTemplate(that.config.template || "ReferenceUniverse");
+  this.team.queue("records").subscribe(function(data) {
+    that.node.html(template(data));
+  }); 
+  widget.autosearch(that);
 });
