d05e75ede48a52fae21f388471acc37ad5e92293
Wolfram Schneider <wosch@indexdata.dk>
2014-07-10 09:06:08 +0000

    working popup function

--- src/mkws-popup.js
+++ src/mkws-popup.js
@@ -4,32 +4,41 @@
 
 // "use strict";
 
-mkws.registerWidgetType('Popup', function() {
-  alert("running popup");
+$(document).ready(function () {
+    // mkws.registerWidgetType('PopupWindow', function() {
+    var $ = mkws.$;
+    var debug = mkws.log;
+    debug("init popup window");
 
-  if (!mkws.$.ui) {
-      alert("Error: jquery-ui.js is missing, did you include it after jQuery core in the HTML file?");
-      return;
-  }
+    if (!$.ui) {
+        alert("Error: jquery-ui.js is missing, did you include it after jQuery core in the HTML file?");
+        return;
+    }
 
-  if (!this.config.popup_width) this.config.popup_width = "800";
-  if (!this.config.popup_height) this.config.popup_height = "600";
-  if (!this.config.auto_open) this.config.auto_open = "0";
+    var popup = $(".PopupWindow");
+    if (!popup) {
+        debug("no popup found");
+        return;
+    } else {
+        debug("found popup windows: " + popup.length);
+    }
 
-  $(this).dialog({
-      closeOnEscape: true,
-      autoOpen: parseInt(this.config.auto_open) ? true : false,
-      height: parseInt(this.config.popup_height),
-      width: parseInt(this.config.popup_width),
-      modal: true,
-      resizable: true,
-      buttons: {
-        Cancel: function() {
-          $(this).dialog("close");
-        }
-      },
-      close: function() { }
-  });
+    var width = parseInt($(popup).attr("popup_width") || "800");
+    var height = parseInt($(popup).attr("popup_height") || "600");
+    var autoOpen = parseInt($(popup).attr("popup_autoOpen") || "0");
 
+    $(popup).dialog({
+        closeOnEscape: true,
+        autoOpen: autoOpen,
+        height: height,
+        width: width,
+        modal: true,
+        resizable: true,
+        buttons: {
+            Cancel: function () {
+                $(popup).dialog("close");
+            }
+        },
+        close: function () {}
+    });
 });
-
