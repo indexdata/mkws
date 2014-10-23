36721667730b5d8aae5b6df304f69d96f2c1d21d
Jason Skomorowski <jason@indexdata.com>
2014-10-23 10:27:39 -0400

    Rename all the templates MKWS-313

--- src/mkws-widget-reference.js
+++ src/mkws-widget-reference.js
@@ -1,7 +1,7 @@
 mkws.registerWidgetType('reference', function() {
   mkws.promotionFunction('record').call(this);
   if (!this.config.target) this.config.target = 'wikimedia_wikipedia_single_result';
-  if (!this.config.template) this.config.template = 'Reference';
+  if (!this.config.template) this.config.template = 'reference';
   this.config.template_vars.paragraphs = this.config.paragraphs || 0;
   this.config.template_vars.sentences = this.config.sentences || 0;
 });
