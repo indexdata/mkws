1adf555f3f913a53b1de6bd5e59e9cdb4fa1932f
Mike Taylor <mike@indexdata.com>
2015-01-12 17:23:27 +0000

    Use template called "wikipedia", not "reference". DEFINTELY the last part of MKWS-349.

--- src/mkws-widget-wikipedia.js
+++ src/mkws-widget-wikipedia.js
@@ -1,7 +1,7 @@
 mkws.registerWidgetType('wikipedia', function() {
   mkws.promotionFunction('record').call(this);
   if (!this.config.target) this.config.target = 'wikimedia_wikipedia_single_result';
-  if (!this.config.template) this.config.template = 'reference';
+  if (!this.config.template) this.config.template = 'wikipedia';
   this.config.template_vars.paragraphs = this.config.paragraphs || 0;
   this.config.template_vars.sentences = this.config.sentences || 0;
   this.config.template_vars.credit = this.config.credit || "Wikipedia";
