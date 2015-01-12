22e681033a9e5111081d701da64536b43b2dd1bb
Mike Taylor <mike@indexdata.com>
2015-01-12 16:51:06 +0000

    Last part of MKWS-348.
    Widget type registered is now "wikipedia".
    But "reference" is still registered as an alias.

--- src/mkws-widget-wikipedia.js
+++ src/mkws-widget-wikipedia.js
@@ -1,4 +1,4 @@
-mkws.registerWidgetType('reference', function() {
+mkws.registerWidgetType('wikipedia', function() {
   mkws.promotionFunction('record').call(this);
   if (!this.config.target) this.config.target = 'wikimedia_wikipedia_single_result';
   if (!this.config.template) this.config.template = 'reference';
@@ -6,3 +6,4 @@ mkws.registerWidgetType('reference', function() {
   this.config.template_vars.sentences = this.config.sentences || 0;
   this.config.template_vars.credit = this.config.credit || "Wikipedia";
 });
+mkws.aliasWidgetType('reference', 'wikipedia');
